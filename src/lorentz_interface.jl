07266b15d1f4f466915f766e1d5fc161c62c0749
Hernandez Acosta, Uwe (FWKH) - 13668 <u.hernandez@hzdr.de>
2022-03-03 00:26:08 +0100

    implemented generic getter for LorentzVectorLike

--- src/lorentz_interface.jl
+++ src/lorentz_interface.jl
@@ -30,8 +30,8 @@ which enables generic functions like:
 ```Julia
 L = CustomType(2.0,1.0,0.0,-1.0)
 
-get_polar_angle(L) # ≈3.1415
-get_rapidity(L) # 
+getTheta(L) # 
+getRapidity(L) # 
 ...
 ```
 """
@@ -59,6 +59,11 @@ function getX end
 function getY end
 function getZ end
 
+function setT! end
+function setX! end
+function setY! end
+function setZ! end
+
 
 
 function register_LorentzVectorLike(T::Type)
@@ -68,32 +73,112 @@ function register_LorentzVectorLike(T::Type)
     _hasmethod_registry(getZ, T)
     
     @eval @traitimpl IsLorentzVectorLike{$T}
+
+    if hasmethod(setT!,Tuple{T})&&hasmethod(setX!,Tuple{T})&&hasmethod(setY!,Tuple{T})&&hasmethod(setZ!,Tuple{T})
+        @eval @traitimpl IsMutableLorentzVectorLike{$T}
+    end
     return 
 end
 
 # general functions
 
+
 @inline @traitfn minkowski_dot(x1::T,x2::T) where {T; IsLorentzVectorLike{T}} = getT(x1)*getT(x2) - (getX(x1)*getX(x2) + getY(x1)*getY(x2) + getZ(x1)*getZ(x2))
+minkowski_dot(x::T1,y::T2) where {T1,T2} = minkowski_dot(promote(x,y)...)
 const mdot = minkowski_dot
 
-@inline @traitfn magnitude2(lv::T) where {T; IsLorentzVectorLike{T}} = getX(lv)^2 + getY(lv)^2 + getZ(lv)^2
-const mag2 = magnitude2
+@inline @traitfn getMagnitude2(lv::T) where {T; IsLorentzVectorLike{T}} = getX(lv)^2 + getY(lv)^2 + getZ(lv)^2
+const getMag2 = getMagnitude2
 
-@inline @traitfn magnitude(lv::T)  where {T; IsLorentzVectorLike{T}} = sqrt(magnitude2(lv))
-const mag = magnitude
+@inline @traitfn getMagnitude(lv::T)  where {T; IsLorentzVectorLike{T}} = sqrt(getMagnitude2(lv))
+const getMag = getMagnitude
 
-@inline @traitfn invariant_mass2(lv::T)  where {T; IsLorentzVectorLike{T}} = minkowski_dot(lv,lv)
-const mass2 = invariant_mass2
+@inline @traitfn getInvariantMass2(lv::T)  where {T; IsLorentzVectorLike{T}} = minkowski_dot(lv,lv)
+const getMass2 = getInvariantMass2
 
-@inline @traitfn invariant_mass(lv::T)  where {T; IsLorentzVectorLike{T}} = sqrt(invariant_mass2(lv))
-const mass = invariant_mass
+@traitfn function getInvariantMass(lv::T)  where {T; IsLorentzVectorLike{T}} 
+    m2 = getInvariantMass2(lv)
+    if m2<zero(m2)
+        # Think about including this waring, maybe optional with a global PRINT_WARINGS switch.
+        #@warn("The square of the invariant mass (m2=P*P) is negative. The value -sqrt(-m2) is returned.")
+        return -sqrt(-m2)
+    else
+        return sqrt(m2)
+    end
+end
+const getMass = getInvariantMass
 
 
 # Momentum specific getter
 
 @inline @traitfn getE(lv::T)  where {T; IsLorentzVectorLike{T}} =  getT(lv)
+const getEnergy = getE
 @inline @traitfn getPx(lv::T)  where {T; IsLorentzVectorLike{T}} =  getX(lv)
 @inline @traitfn getPy(lv::T)  where {T; IsLorentzVectorLike{T}} =  getY(lv)
 @inline @traitfn getPz(lv::T)  where {T; IsLorentzVectorLike{T}} =  getZ(lv)
 
+@inline @traitfn function getBeta(lv::T)  where {T; IsLorentzVectorLike{T}}
+    if getT(lv)!=zero(getT(lv))
+        getRho(lv)/getT(lv)
+    else
+        throw(error("There is no beta for a LorentzVectorLike with vanishing time/energy component"))
+    end
+end
+
+@inline @traitfn getGamma(lv::T)  where {T; IsLorentzVectorLike{T}} =  inv(sqrt(one(getT(lv))-getBeta(lv)^2))
+
+# transverse coordinates
+@inline @traitfn getTransverseMomentum2(lv::T)  where {T; IsLorentzVectorLike{T}} = getX(lv)^2 + getY(lv)^2
+const getPt2 = getTransverseMomentum2
+const getPerp2 = getTransverseMomentum2
+@inline @traitfn getTransverseMomentum(lv::T)  where {T; IsLorentzVectorLike{T}} = sqrt(getTransverseMomentum2(lv))
+const getPt = getTransverseMomentum
+const getPerp = getTransverseMomentum
+
+@inline @traitfn getTransverseMass2(lv::T)  where {T; IsLorentzVectorLike{T}} = getT(lv)^2 - getZ(lv)^2
+const getMt2 = getTransverseMass2
+@traitfn function getTransverseMass(lv::T)  where {T; IsLorentzVectorLike{T}}
+    mT2 = getTransverseMass2(lv)
+    if mT2<zero(mT2)
+        # add optional waring: negative transverse mass -> -sqrt(-mT2) is returned.
+        -sqrt(-mT2)
+    else
+        sqrt(mT2)
+    end
+end
+const getMt = getTransverseMass
+
+
+@inline @traitfn getRapidity(lv::T)  where {T; IsLorentzVectorLike{T}} = 0.5*log((getT(lv) + getZ(lv))/(getT(lv) - getZ(lv)))
+
 # spherical coordinates
+const getRho2 = getMagnitude2
+const getRho = getMagnitude 
+
+@inline @traitfn function getTheta(lv::T)  where {T; IsLorentzVectorLike{T}}
+    getX(lv)==zero(getX(lv)) && getY(lv)==zero(getY(lv)) && getZ(lv)==zero(getZ(lv)) ? zero(getX(lv)) : atan(getTransverseMomentum(lv),getZ(lv))
+end
+
+@traitfn function getCosTheta(lv::T)  where {T; IsLorentzVectorLike{T}}
+    r = getRho(lv)
+    r==zero(getX(lv)) ? zero(getX(lv)) : getZ(lv)/r
+end
+
+@traitfn function getPhi(lv::T)  where {T; IsLorentzVectorLike{T}}
+    n = zero(getX(lv))
+    getX(lv)==n && getY(lv)==n ? n : atan(getY(lv),getX(lv))
+end
+
+
+# light cone coordinates
+@inline @traitfn getPlus(lv::T)  where {T; IsLorentzVectorLike{T}} = 0.5*(getT(lv) + getZ(lv))
+@inline @traitfn getMinus(lv::T)  where {T; IsLorentzVectorLike{T}} = 0.5*(getT(lv) - getZ(lv))
+
+
+####
+#
+# Setter
+#
+####
+
+@inline @traitfn setPy!(lv::T,value::VT)  where {T,VT; IsLorentzVectorLike{T}} =  setY!(lv,value)
--- src/test.jl
+++ src/test.jl
@@ -10,10 +10,10 @@ struct CustomType<:FieldVector{4,Float64}
     z::Float64
 end
 
-QEDbase.getT(lv::CustomType) = lv.t
-QEDbase.getX(lv::CustomType) = lv.x
-QEDbase.getY(lv::CustomType) = lv.y
-QEDbase.getZ(lv::CustomType) = lv.z
+@inline QEDbase.getT(lv::CustomType) = lv.t
+@inline QEDbase.getX(lv::CustomType) = lv.x
+@inline QEDbase.getY(lv::CustomType) = lv.y
+@inline QEDbase.getZ(lv::CustomType) = lv.z
 
 
 #QEDbase.LorentzVectorStyle(::CustomType) = QEDbase.IsLorentzVectorLike()
@@ -31,3 +31,127 @@ b = CustomType(rand(4)...)
 mdot(a,b)
 
 @benchmark mdot($a,$b)
+
+m = 1.0
+E = 1.2
+rho = sqrt(E^2 - m^2)
+th = 0.2
+ph = 0.1
+
+P = CustomType(E,rho*cos(ph)*sin(th),rho*sin(ph)*sin(th),rho*cos(th))
+
+@benchmark QEDbase.getPhi($P)
+
+@benchmark QEDbase.getTheta($P)
+
+@benchmark QEDbase.getMass($P)
+
+iP = CustomType(1.0,0,0,2.0)
+
+@benchmark QEDbase.getMass($iP)
+
+
+@benchmark QEDbase.getGamma($P)
+
+@benchmark QEDbase.getPlus($P)
+@benchmark QEDbase.getMinus($P)
+
+@benchmark QEDbase.getRapidity($P)
+
+
+mutable struct CustomType2
+    t::Float64
+    x::Float64
+    y::Float64
+    z::Float64
+end
+
+@inline QEDbase.getT(lv::CustomType2) = lv.t
+@inline QEDbase.getX(lv::CustomType2) = lv.x
+@inline QEDbase.getY(lv::CustomType2) = lv.y
+@inline QEDbase.getZ(lv::CustomType2) = lv.z
+
+
+# needs to be implemented if it shall work together with other LorentzVectorLike 
+Base.convert(::Type{CustomType2},x::CustomType) = CustomType2(x.t,x.x,x.y,x.z)
+Base.promote_rule(::Type{CustomType2},::Type{CustomType}) = CustomType2
+
+QEDbase.register_LorentzVectorLike(CustomType2)
+
+m = 1.0
+E = 1.2
+rho = sqrt(E^2 - m^2)
+th = 0.2
+ph2 = 0.1
+
+
+mP = CustomType2(E,rho*cos(ph2)*sin(th),rho*sin(ph2)*sin(th),rho*cos(th))
+
+@benchmark QEDbase.getPlus($mP)
+@benchmark QEDbase.getRapidity($mP)
+
+
+
+@benchmark QEDbase.mdot($mP,$P)
+
+QEDbase.mdot(mP,P)
+
+
+mutable struct CustomType3 <: FieldVector{4,Float64}
+    t::Float64
+    x::Float64
+    y::Float64
+    z::Float64
+end
+
+@inline QEDbase.getT(lv::CustomType3) = lv.t
+@inline QEDbase.getX(lv::CustomType3) = lv.x
+@inline QEDbase.getY(lv::CustomType3) = lv.y
+@inline QEDbase.getZ(lv::CustomType3) = lv.z
+
+function QEDbase.setT!(lv::CustomType3,value::Float64)
+    lv.t = value
+end
+
+function QEDbase.setX!(lv::CustomType3,value::Float64)
+    lv.x = value
+end
+
+function QEDbase.setY!(lv::CustomType3,value::Float64)
+    lv.y = value
+end
+
+function QEDbase.setZ!(lv::CustomType3,value::Float64)
+    lv.z = value
+end
+
+
+Base.promote_rule(::Type{CustomType3},::Type{CustomType}) = CustomType3
+
+QEDbase.register_LorentzVectorLike(CustomType3)
+
+m = 1.0
+E = 1.2
+rho = sqrt(E^2 - m^2)
+th = 0.2
+ph2 = 0.1
+
+
+smP = CustomType3(E,rho*cos(ph2)*sin(th),rho*sin(ph2)*sin(th),rho*cos(th))
+
+
+newY = 4.0
+@benchmark QEDbase.setPy!($smP,$newY)
+
+
+QEDbase.mdot(smP,smP)
+
+@code_lowered QEDbase.mdot(mP,P)
+@code_typed QEDbase.mdot(mP,P)
+@code_warntype QEDbase.mdot(mP,P)
+@code_llvm QEDbase.mdot(mP,P)
+@code_native QEDbase.mdot(P,P)
+
+
+
+
