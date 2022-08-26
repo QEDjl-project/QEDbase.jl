cc3c8c5b388f0d7e461da1af9894ad08f63eb950
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2022-08-26 09:07:11 +0200

    Corrected Gamma matrices and add test for correct matrix definition

--- test/gamma_matrices.jl
+++ test/gamma_matrices.jl
@@ -1,6 +1,7 @@
 
 using LinearAlgebra
 using Random
+using SparseArrays
 
 function _gamma_anticommutator(i, j)
     return GAMMA[i] * GAMMA[j] + GAMMA[j] * GAMMA[i]
@@ -12,6 +13,29 @@ end
 
 METRIC = Diagonal([1, -1, -1, -1])
 EYE = one(DiracMatrix)
+GROUNDTRUTH_GAMMA0_DIRAC = spzeros(4, 4)
+GROUNDTRUTH_GAMMA0_DIRAC[1, 1] = 1
+GROUNDTRUTH_GAMMA0_DIRAC[2, 2] = 1
+GROUNDTRUTH_GAMMA0_DIRAC[3, 3] = -1
+GROUNDTRUTH_GAMMA0_DIRAC[4, 4] = -1
+
+GROUNDTRUTH_GAMMA1_DIRAC = spzeros(4, 4)
+GROUNDTRUTH_GAMMA1_DIRAC[4, 1] = -1
+GROUNDTRUTH_GAMMA1_DIRAC[3, 2] = -1
+GROUNDTRUTH_GAMMA1_DIRAC[2, 3] = 1
+GROUNDTRUTH_GAMMA1_DIRAC[1, 4] = 1
+
+GROUNDTRUTH_GAMMA2_DIRAC = spzeros(ComplexF64, 4, 4)
+GROUNDTRUTH_GAMMA2_DIRAC[4, 1] = -1im
+GROUNDTRUTH_GAMMA2_DIRAC[3, 2] = 1im
+GROUNDTRUTH_GAMMA2_DIRAC[2, 3] = 1im
+GROUNDTRUTH_GAMMA2_DIRAC[1, 4] = -1im
+
+GROUNDTRUTH_GAMMA3_DIRAC = spzeros(4, 4)
+GROUNDTRUTH_GAMMA3_DIRAC[3, 1] = -1
+GROUNDTRUTH_GAMMA3_DIRAC[4, 2] = 1
+GROUNDTRUTH_GAMMA3_DIRAC[1, 3] = 1
+GROUNDTRUTH_GAMMA3_DIRAC[2, 4] = -1
 
 @testset "gamma matrices" begin
     rng = MersenneTwister(42)
@@ -56,5 +80,33 @@ EYE = one(DiracMatrix)
 
         @test isapprox(slashed(a), GAMMA * a)
         @test isapprox(slashed(a), slashed(DiracGammaRepresentation, a))
+    end
+
+    @testset "Dirac representation" begin
+        # check the components of the gamma matrices against the 
+        # Dirac representations, e.g. from https://en.wikipedia.org/wiki/Gamma_matrices
+        # note: we use the convention of lower indices for the gamma matrix definition.
+        #       This motivates the minus sign in front of the spatial components
+        comps = 1:4
+        @testset "gamma_0" begin
+            @testset "($col,$row)" for (row, col) in Iterators.product(comps, comps)
+                @test isapprox(GAMMA[1][row, col], GROUNDTRUTH_GAMMA0_DIRAC[row, col])
+            end
+        end
+        @testset "gamma_1" begin
+            @testset "($col,$row)" for (row, col) in Iterators.product(comps, comps)
+                @test isapprox(GAMMA[2][row, col], -GROUNDTRUTH_GAMMA1_DIRAC[row, col])
+            end
+        end
+        @testset "gamma_2" begin
+            @testset "($col,$row)" for (row, col) in Iterators.product(comps, comps)
+                @test isapprox(GAMMA[3][row, col], -GROUNDTRUTH_GAMMA2_DIRAC[row, col])
+            end
+        end
+        @testset "gamma_3" begin
+            @testset "($col,$row)" for (row, col) in Iterators.product(comps, comps)
+                @test isapprox(GAMMA[4][row, col], -GROUNDTRUTH_GAMMA3_DIRAC[row, col])
+            end
+        end
     end
 end
