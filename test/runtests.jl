2c005143752040057d6df49ad97863f9a243bd1c
Anton Reinhard <s1509337@msx.tu-dresden.de>
2023-10-10 16:58:58 +0200

    Move particle definitions from QEDprocesses.jl to QEDbase.jl (#25)
    Co-authored-by: Anton Reinhard <anton.reinhard@proton.me>
    Co-authored-by: AntonReinhard <anton.reinhard@protonmail.com>
    Co-authored-by: Uwe Hernandez Acosta <szabo137@users.noreply.github.com>
    Co-authored-by: Tom Jungnickel <140055258+tjungni@users.noreply.github.com>

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,8 +11,13 @@ using Test
     include("particle_spinors.jl")
     include("four_momentum.jl")
 <<<<<<< HEAD
+<<<<<<< HEAD
 
     include("particles.jl")
 =======
 >>>>>>> baec5cc (Enhancement for the gitlab-ci)
+=======
+
+    include("particles.jl")
+>>>>>>> aa78a7d (Move particle definitions from QEDprocesses.jl to QEDbase.jl (#25))
 end
