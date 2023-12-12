285d62dd7e11e10728b30e1c3fee4a68d7cd16e6
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2023-12-12 15:20:41 +0100

    formatting

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,30 +3,30 @@ using Test
 using SafeTestsets
 
 begin
-    @time @safetestset "Dirac tensors" begin    
+    @time @safetestset "Dirac tensors" begin
         include("dirac_tensor.jl")
     end
-        
-    @time @safetestset "Lorentz Vectors" begin    
+
+    @time @safetestset "Lorentz Vectors" begin
         include("lorentz_vector.jl")
     end
-        
-    @time @safetestset "Lorentz interface" begin    
+
+    @time @safetestset "Lorentz interface" begin
         include("lorentz_interface.jl")
     end
-    @time @safetestset "Gamma matrices" begin    
+    @time @safetestset "Gamma matrices" begin
         include("gamma_matrices.jl")
     end
 
-    @time @safetestset "particle spinors" begin    
+    @time @safetestset "particle spinors" begin
         include("particle_spinors.jl")
     end
-        
-    @time @safetestset "four momentum" begin    
+
+    @time @safetestset "four momentum" begin
         include("four_momentum.jl")
     end
 
-    @time @safetestset "particles" begin    
+    @time @safetestset "particles" begin
         include("particles.jl")
     end
 end
