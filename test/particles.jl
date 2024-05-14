bf934fabd8a9d69625a9a619e90f0ab3c3f232ae
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2024-05-14 13:33:00 +0200

    added scalar broadcasting to spins and polarizations

--- test/particles.jl
+++ test/particles.jl
@@ -34,11 +34,22 @@ X, Y, Z = rand(RNG, 3)
 # test function to test scalar broadcasting
 test_broadcast(x::AbstractParticle) = x
 test_broadcast(x::ParticleDirection) = x
+test_broadcast(x::AbstractSpinOrPolarization) = x
 
-@testset "broadcast directions" begin
-    @testset "dir" for dir in (Incoming(),Outgoing())
-        @test test_broadcast.(dir) == dir
+
+@testset "scalar broadcasting" begin
+    @testset "directions" begin
+        @testset "dir" for dir in (Incoming(),Outgoing())
+            @test test_broadcast.(dir) == dir
+        end
     end
+
+    @testset "spins and polarization" begin
+        @testset "spin_or_pol" for spin_or_pol in (SpinUp(),SpinDown(),AllSpin(),PolX(),PolY(),AllPol())
+            @test test_broadcast.(spin_or_pol) == spin_or_pol 
+        end
+    end
+
 end
 
 @testset "fermion likes" begin
