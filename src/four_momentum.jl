a949f491aa8f6549a896da49ad9d275bd787e9f4
Anton Reinhard <anton.reinhard@proton.me>
2024-05-27 13:39:24 +0200

    Add a little documentation to AFM

--- src/four_momentum.jl
+++ src/four_momentum.jl
@@ -1,6 +1,9 @@
 """
-SFourMomentum type
+    AbstractFourMomentum
 
+Abstract base type for four-momentas, representing one energy and three spacial components.
+
+Also see: [`SFourMomentum`](@ref)
 """
 
 abstract type AbstractFourMomentum <: AbstractLorentzVector{Float64} end
