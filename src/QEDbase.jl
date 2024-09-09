88844420ea920f73939cd867b49c744f863bcbf5
AntonReinhard <anton.reinhard@proton.me>
2024-09-09 13:31:27 +0200

    Add tests

--- src/QEDbase.jl
+++ src/QEDbase.jl
@@ -45,7 +45,7 @@ export AbstractDefinitePolarization, AbstractIndefinitePolarization
 export PolarizationX, PolX, PolarizationY, PolY, AllPolarization, AllPol
 export AbstractDefiniteSpin, AbstractIndefiniteSpin
 export SpinUp, SpinDown, AllSpin
-export SyncedSpin, SyncedPolarization
+export SyncedSpin, SyncedPolarization, SyncedPol
 export spin_pols_iter
 
 # probabilities
