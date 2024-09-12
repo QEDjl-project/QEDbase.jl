25ce9bd51d05babf26e0a0cceb66eaf46567a846
Anton Reinhard <anton.reinhard@proton.me>
2024-09-12 12:55:05 +0200

    Add spin_pols_iter to iterate over a process' spin/pol combinations (#118)
    As the title says, this adds an iterator to yield all possible
    combinations of spins and polarizations allowed by a process' set
    `spin_pols()`. For example:
    
    ```Julia
    julia> using QEDbase; using QEDcore; using QEDprocesses;
    
    julia> proc = ScatteringProcess((Photon(), Photon(), Photon(), Electron()), (Photon(), Electron()), (SyncedPolarization(1), SyncedPolarization(2), SyncedPolarization(1), SpinUp()), (SyncedPolarization(2), AllSpin()))
    generic QED process
        incoming: photon (synced polarization 1), photon (synced polarization 2), photon (synced polarization 1), electron (spin up)
        outgoing: photon (synced polarization 2), electron (all spins)
    
    
    julia> for sp_combo in spin_pols_iter(proc) println(sp_combo) end
    ((x-polarized, x-polarized, x-polarized, spin up), (x-polarized, spin up))
    ((y-polarized, x-polarized, y-polarized, spin up), (x-polarized, spin up))
    ((x-polarized, y-polarized, x-polarized, spin up), (y-polarized, spin up))
    ((y-polarized, y-polarized, y-polarized, spin up), (y-polarized, spin up))
    ((x-polarized, x-polarized, x-polarized, spin up), (x-polarized, spin down))
    ((y-polarized, x-polarized, y-polarized, spin up), (x-polarized, spin down))
    ((x-polarized, y-polarized, x-polarized, spin up), (y-polarized, spin down))
    ((y-polarized, y-polarized, y-polarized, spin up), (y-polarized, spin down))
    
    julia> length(spin_pols_iter(proc))
    8
    ```
    The above is also a `jldoctest`.
    
    As a side-note I also added an alias of `SyncedPol` to
    `SyncedPolarization`.
    
    The code is not incredibly concise and also not incredibly fast, but for
    the reasonable cases that I tested `@benchmark` reports well under 1ms.
    Since I don't think this iterator would be the critical path of anything
    this should be fine.
    The only problem I could see is that due to everything using `Tuple`s in
    its arguments, the compile time is relatively large. If this becomes a
    problem we could change it to using `Vector`s instead, likely trading
    some runtime for much better compile time.
    
    Fixes #107

--- src/QEDbase.jl
+++ src/QEDbase.jl
@@ -45,7 +45,8 @@ export AbstractDefinitePolarization, AbstractIndefinitePolarization
 export PolarizationX, PolX, PolarizationY, PolY, AllPolarization, AllPol
 export AbstractDefiniteSpin, AbstractIndefiniteSpin
 export SpinUp, SpinDown, AllSpin
-export SyncedSpin, SyncedPolarization
+export SyncedSpin, SyncedPolarization, SyncedPol
+export spin_pols_iter
 
 # probabilities
 export differential_probability, unsafe_differential_probability
@@ -114,6 +115,7 @@ include("interfaces/phase_space_point.jl")
 include("implementations/process/momenta.jl")
 include("implementations/process/particles.jl")
 include("implementations/process/spin_pols.jl")
+include("implementations/process/spin_pol_iterator.jl")
 
 include("implementations/cross_section/diff_probability.jl")
 include("implementations/cross_section/diff_cross_section.jl")
