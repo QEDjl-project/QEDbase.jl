d02100a2057dc9bd97fee6c522b56de0bb40f5c4
AntonReinhard <anton.reinhard@proton.me>
2024-10-06 12:35:27 +0200

    Add assumptions to make vector and matrix *() :total

--- src/interfaces/lorentz_vectors/dirac_interaction.jl
+++ src/interfaces/lorentz_vectors/dirac_interaction.jl
@@ -11,7 +11,9 @@ Product of generic Lorentz vector with a Dirac tensor from the left. Basically, 
 function _mul(
     DM::T, L::TL
 ) where {T<:Union{AbstractDiracMatrix,AbstractDiracVector},TL<:AbstractLorentzVector}
-    return constructorof(TL)(DM * L[1], DM * L[2], DM * L[3], DM * L[4])
+    # constructorof doesn't guarantee inaccessiblememonly but it is safe to assume here
+    Base.@assume_effects :inaccessiblememonly
+    return @inbounds constructorof(TL)(DM * L[1], DM * L[2], DM * L[3], DM * L[4])
 end
 @inline function *(
     DM::T, L::TL
@@ -31,7 +33,9 @@ Product of generic Lorentz vector with a Dirac tensor from the right. Basically,
 function _mul(
     L::TL, DM::T
 ) where {TL<:AbstractLorentzVector,T<:Union{AbstractDiracMatrix,AbstractDiracVector}}
-    return constructorof(TL)(L[1] * DM, L[2] * DM, L[3] * DM, L[4] * DM)
+    # constructorof doesn't guarantee inaccessiblememonly but it is safe to assume here
+    Base.@assume_effects :inaccessiblememonly
+    return @inbounds constructorof(TL)(L[1] * DM, L[2] * DM, L[3] * DM, L[4] * DM)
 end
 @inline function *(
     L::TL, DM::T
