08e6d0d300c5e0a27198a538821799424b35fdb1
Uwe Hernandez Acosta <u.hernandez@hzdr.de>
2025-01-06 22:02:49 +0100

    added several momentum eltypes to coordinate trafo tests

--- test/interfaces/coordinate_transforms.jl
+++ test/interfaces/coordinate_transforms.jl
@@ -9,48 +9,61 @@ include("../test_implementation/TestImplementation.jl")
 using .TestImplementation
 
 TESTMODEL = TestModel()
-TESTPSDEF = TestPhasespaceDef{TestMomentum}()
 
-TESTTRAFO = TestCoordinateTrafo()
+@testset "$MOM_EL_TYPE" for MOM_EL_TYPE in (Float16, Float32, Float64)
+    MOM_TYPE = TestMomentum{MOM_EL_TYPE}
+    TESTPSDEF = TestPhasespaceDef{MOM_TYPE}()
 
-@testset "broadcast" begin
-    test_func(trafo) = trafo
-    @test test_func.(TESTTRAFO) == TESTTRAFO
-end
+    TESTTRAFO = TestCoordinateTrafo()
 
-@testset "single momenta" begin
-    test_mom = TestMomentum(rand(RNG, 4))
+    @testset "broadcast" begin
+        test_func(trafo) = trafo
+        @test test_func.(TESTTRAFO) == TESTTRAFO
+    end
 
-    test_mom_prime = @inferred TESTTRAFO(test_mom)
+    @testset "single momenta" begin
+        test_mom = MOM_TYPE(rand(RNG, 4))
 
-    @test isapprox(test_mom_prime, TestImplementation._groundtruth_coord_trafo(test_mom))
-end
+        test_mom_prime = @inferred TESTTRAFO(test_mom)
 
-@testset "set of momenta" begin
-    test_moms = [TestMomentum(rand(RNG, 4)) for _ in 1:3]
-    test_moms_prime = TESTTRAFO.(test_moms)
-
-    @test isapprox(test_moms_prime, TestImplementation._groundtruth_coord_trafo.(test_moms))
-end
-
-@testset "phase space points" begin
-    @testset "($N_INCOMING,$N_OUTGOING)" for (N_INCOMING, N_OUTGOING) in Iterators.product(
-        (1, rand(RNG, 2:8)), (1, rand(RNG, 2:8))
-    )
-        INCOMING_PARTICLES = Tuple(rand(RNG, TestImplementation.PARTICLE_SET, N_INCOMING))
-        OUTGOING_PARTICLES = Tuple(rand(RNG, TestImplementation.PARTICLE_SET, N_OUTGOING))
-
-        TESTPROC = TestProcess(INCOMING_PARTICLES, OUTGOING_PARTICLES)
-
-        p_in_phys = TestImplementation._rand_momenta(RNG, N_INCOMING, TestMomentum)
-        p_out_phys = TestImplementation._rand_momenta(RNG, N_OUTGOING, TestMomentum)
-
-        PS_POINT = TestPhaseSpacePoint(
-            TESTPROC, TESTMODEL, TESTPSDEF, p_in_phys, p_out_phys
+        @test isapprox(
+            test_mom_prime, TestImplementation._groundtruth_coord_trafo(test_mom)
         )
+    end
 
-        test_psp_prime = TESTTRAFO(PS_POINT)
+    @testset "set of momenta" begin
+        test_moms = [MOM_TYPE(rand(RNG, 4)) for _ in 1:3]
+        test_moms_prime = TESTTRAFO.(test_moms)
 
-        @test test_psp_prime == TestImplementation._groundtruth_coord_trafo(PS_POINT)
+        @test isapprox(
+            test_moms_prime, TestImplementation._groundtruth_coord_trafo.(test_moms)
+        )
+    end
+
+    @testset "phase space points" begin
+        @testset "($N_INCOMING,$N_OUTGOING)" for (N_INCOMING, N_OUTGOING) in
+                                                 Iterators.product(
+            (1, rand(RNG, 2:8)), (1, rand(RNG, 2:8))
+        )
+            INCOMING_PARTICLES = Tuple(
+                rand(RNG, TestImplementation.PARTICLE_SET, N_INCOMING)
+            )
+            OUTGOING_PARTICLES = Tuple(
+                rand(RNG, TestImplementation.PARTICLE_SET, N_OUTGOING)
+            )
+
+            TESTPROC = TestProcess(INCOMING_PARTICLES, OUTGOING_PARTICLES)
+
+            p_in_phys = TestImplementation._rand_momenta(RNG, N_INCOMING, MOM_TYPE)
+            p_out_phys = TestImplementation._rand_momenta(RNG, N_OUTGOING, MOM_TYPE)
+
+            PS_POINT = TestPhaseSpacePoint(
+                TESTPROC, TESTMODEL, TESTPSDEF, p_in_phys, p_out_phys
+            )
+
+            test_psp_prime = TESTTRAFO(PS_POINT)
+
+            @test test_psp_prime == TestImplementation._groundtruth_coord_trafo(PS_POINT)
+        end
     end
 end
