6cbef9112229e24ab949bd7e0586ad6316505af1
AntonReinhard <anton.reinhard@proton.me>
2025-02-28 18:35:22 +0100

    Add eltype-esque functions, make FourMomentum generic
    momentum_type and momentum_eltype for psp and ps, eltype for AbstractFourMomentum

--- src/interfaces/four_momentum.jl
+++ src/interfaces/four_momentum.jl
@@ -1,11 +1,11 @@
 """
-    AbstractFourMomentum
+    AbstractFourMomentum{T_ELEM} where {T_ELEM<:Real}
 
 Abstract base type for four-momentas, representing one energy and three spacial components.
 
 Also see: [`QEDcore.SFourMomentum`](@extref), [`QEDcore.MFourMomentum`](@extref)
 """
-abstract type AbstractFourMomentum <: AbstractLorentzVector{Float64} end
+abstract type AbstractFourMomentum{T_ELEM<:Real} <: AbstractLorentzVector{T_ELEM} end
 
 function Base.getproperty(P::TM, sym::Symbol) where {TM<:AbstractFourMomentum}
     if sym == :t
@@ -20,3 +20,6 @@ function Base.getproperty(P::TM, sym::Symbol) where {TM<:AbstractFourMomentum}
         return getfield(P, sym)
     end
 end
+
+@inline Base.eltype(::Type{AbstractFourMomentum{T}}) where {T} = T
+@inline Base.eltype(P::AbstractFourMomentum) = eltype(typeof(P))
